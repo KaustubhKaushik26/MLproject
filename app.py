f8a7c3de70b681fbc02f3e159340a04e47bf4187
Kaustubh Kaushik <kaustubhkaushik26@gmail.com>
2024-11-14 23:41:55 +0530

    updation in project

---
+++ app.py
@@ -0,0 +1,6 @@
+from src.mlproject.logger import log as logging
+from src.mlproject.exception import CustomException
+import sys
+if __name__=='__main__':
+    logging.info('The execution has started')
+
