5df3575fcad705d71f630661d8780e6b87538a53
Kaustubh Kaushik <kaustubhkaushik26@gmail.com>
2024-11-17 13:33:07 +0530

    ModelTraining

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sys
 from src.mlproject.component.data_ingestion import DataIngestion
 from src.mlproject.component.data_ingestion import DataIngestionConfig
 from src.mlproject.component.data_tranformation import DataTranformationConfig,DataTranformation
-
+from src.mlproject.component.model_trainer import ModelTrainerConfig,ModelTrainer
 
 if __name__=='__main__':
     logging.info('The execution has started')
@@ -17,7 +17,11 @@ if __name__=='__main__':
         
         #data_tranformation_config = DataIngestionConfig()
         data_tranformation=DataTranformation()
-        data_tranformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,preprocessor_path = data_tranformation.initiate_data_transformation(train_data_path,test_data_path)
+        
+        #Model training code
+        model_trianer = ModelTrainer()
+        print(model_trianer.initiate_model_trainer(train_arr,test_arr))
       
     except Exception as e:
         logging.info("Custom Exception")
