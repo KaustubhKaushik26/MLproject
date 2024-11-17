4d6867562e0b451668e8b56c6a82b10f2df96aa9
Kaustubh Kaushik <kaustubhkaushik26@gmail.com>
2024-11-17 22:50:27 +0530

    MLflow

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## End to End Data Science project
+
+import dagshub
+dagshub.init(repo_owner='kaustubhkaushik26', repo_name='MLproject', mlflow=True)
+
+import mlflow
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
--- src/mlproject/component/model_trainer.py
+++ src/mlproject/component/model_trainer.py
@@ -2,6 +2,7 @@
 import os
 import sys
 from dataclasses import dataclass
+from urllib.parse import urlparse
 from catboost import CatBoostRegressor
 from sklearn. ensemble import (
     AdaBoostRegressor,
@@ -9,15 +10,17 @@ from sklearn. ensemble import (
     RandomForestRegressor,
     )
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from xgboost import XGBRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+import numpy as np
 
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import log as logging
 
 from src.mlproject.utils import save_object,evaluate_models
+import mlflow
 
 @dataclass
 class ModelTrainerConfig:
@@ -26,6 +29,22 @@ class ModelTrainerConfig:
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
+        
+    def eval_metrics(self, y_true, y_pred):
+        try:
+            # RMSE calculation
+            rmse = np.sqrt(mean_squared_error(y_true, y_pred))
+            
+            # MAE calculation
+            mae = mean_absolute_error(y_true, y_pred)
+            
+            # R2 calculation
+            r2 = r2_score(y_true, y_pred)
+
+            return rmse, mae, r2
+        
+        except Exception as e:
+            raise CustomException(f"Error in evaluation metrics: {str(e)}", sys)
         
     def initiate_model_trainer(self,train_array,test_array):
         try:
@@ -96,6 +115,51 @@ class ModelTrainer:
                 list (model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
+            print("This is the best model")
+            print(best_model_name)
+            
+            model_names = list(params.keys())
+            
+            actuall_model=""
+            
+            for model in model_names:
+                if best_model_name == model:
+                    actuall_model = actuall_model + model
+                    
+            best_params = params[actuall_model]
+            
+            mlflow.set_registry_uri("https://dagshub.com/kaustubhkaushik26/MLproject.mlflow")
+            tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+            
+            # ML FLOW 
+            input_example = X_train[0].reshape(1, -1)  # Select a single sample and reshape if needed
+            
+            with mlflow.start_run():
+
+                predicted_qualities = best_model.predict(X_test)
+
+                (rmse, mae, r2) = self.eval_metrics(y_test, predicted_qualities)
+
+                mlflow.log_params(best_params)
+
+                mlflow.log_metric("rmse", rmse)
+                mlflow.log_metric("r2", r2)
+                mlflow.log_metric("mae", mae)
+
+                # Log the model with input example
+                mlflow.sklearn.log_model(best_model, "model", input_example=input_example)
+
+                # Model registry does not work with file store
+                if tracking_url_type_store != "file":
+
+                    # Register the model
+                    # There are other ways to use the Model Registry, which depends on the use case,
+                    # please refer to the doc for more information:
+                    # https://mlflow.org/docs/latest/model-registry.html#api-workflow
+                    mlflow.sklearn.log_model(best_model, "model", registered_model_name=actual_model)
+                else:
+                    mlflow.sklearn.log_model(best_model, "model")
+            
             
             
             if best_model_score<0.6:
